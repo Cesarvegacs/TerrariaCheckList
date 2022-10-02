3beb13b801edff1f113111133969eb215f179a60
RasecTon <cesar.avm@outlook.com>
2022-10-01 23:36:29 -0700

    responsive fix

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+if (window.innerWidth < 768) {
+        document.getElementById("drunk-world").classList.remove("offset-1");
+        document.getElementById("not-the-bees").classList.remove("offset-2");
+        document.getElementById("for-the-worthy").classList.remove("offset-1");
+        document.getElementById("celebration-mk10").classList.remove("offset-2");
+        document.getElementById("the-constant").classList.remove("offset-1");
+        document.getElementById("no-traps").classList.remove("offset-2");
+        document.getElementById("remix").classList.remove("offset-1");
+        document.getElementById("zenith").classList.remove("offset-2");
+    }
